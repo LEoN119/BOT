81a80b8eb3143f7e7609f16a857e060f99f1c28d
LEoN119 <51816320+LEoN119@users.noreply.github.com>
2019-07-12 14:44:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3534,5 +3534,21 @@ client.on('message', async(message) => {
     }
 });
 
+    client.on('guildMemberRemove', member => {
+        var embed = new Discord.RichEmbed()
+        .setAuthor(member.user.username, member.user.avatarURL)
+        .setThumbnail(member.user.avatarURL)
+        .setTitle(`لقد غادر السيرفر ✋:skin-tone-1: 😔`)
+        .setDescription(`لقد غادر السيرفر ✋:skin-tone-1: 😔 `)
+        .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+        .setColor('RED')
+        .setFooter(`==== نــتــمــنــآ لــكــم آســتــمـــتــآع ====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
+   
+    var channel =member.guild.channels.find('name', 'welcome')
+    if (!channel) return;
+    channel.send({embed : embed});
+    })
+
+
 client.login(process.env.BOT_TOKEN)
 
